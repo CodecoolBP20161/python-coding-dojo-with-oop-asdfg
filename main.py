4cb1320b9fc1b8a6ea1af3303ca9af0314fdb4d8
TBalint19 <toth910719balint@gmail.com>
2016-09-13 09:56:49 +0200

    5

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+class ContactList():
 
-class Contact:
-    pass
+    
+
+
+class Contact():
+
+    all_contacts = []
+
+    def __init__(self, name, email):
+
+        self.name = name
+        self.email = email
+        self.__class__.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+
+
+class Supplier(Contact):
+
+    all_contacts = []
+    all_orders = {}
+
+    def __init__(self, name, email):
+        super().__init__(name, email)
+
+    def order(self, string):
+        self.__class__.all_orders.update({self.email: string})
+
+x = Supplier(1, 2)
+x.order("ddd")
+print(Supplier.all_orders)
